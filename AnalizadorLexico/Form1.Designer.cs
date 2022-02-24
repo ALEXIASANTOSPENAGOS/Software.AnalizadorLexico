0ea190ed6c16de812a765b15c236f4e6e87710fb
ALEXIASANTOSPENAGOS <alexia.santos52@unach.mx>
2022-02-24 13:01:39 -0600

    Este es un analizador lexico

---
+++ AnalizadorLexico/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace AnalizadorLexico
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtEntrada = new System.Windows.Forms.TextBox();
+            this.btnAnalizar = new System.Windows.Forms.Button();
+            this.txtSalida = new System.Windows.Forms.TextBox();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(38, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(224, 36);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Código Fuente";
+            // 
+            // txtEntrada
+            // 
+            this.txtEntrada.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEntrada.Location = new System.Drawing.Point(44, 151);
+            this.txtEntrada.Multiline = true;
+            this.txtEntrada.Name = "txtEntrada";
+            this.txtEntrada.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtEntrada.Size = new System.Drawing.Size(550, 350);
+            this.txtEntrada.TabIndex = 1;
+            // 
+            // btnAnalizar
+            // 
+            this.btnAnalizar.BackColor = System.Drawing.Color.GhostWhite;
+            this.btnAnalizar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnAnalizar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnAnalizar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAnalizar.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.btnAnalizar.ForeColor = System.Drawing.Color.Black;
+            this.btnAnalizar.Location = new System.Drawing.Point(44, 515);
+            this.btnAnalizar.Name = "btnAnalizar";
+            this.btnAnalizar.Size = new System.Drawing.Size(326, 54);
+            this.btnAnalizar.TabIndex = 2;
+            this.btnAnalizar.Text = "Analizar Tokens";
+            this.btnAnalizar.UseVisualStyleBackColor = false;
+            this.btnAnalizar.Click += new System.EventHandler(this.btnAnalizar_Click);
+            // 
+            // txtSalida
+            // 
+            this.txtSalida.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSalida.Location = new System.Drawing.Point(688, 151);
+            this.txtSalida.Multiline = true;
+            this.txtSalida.Name = "txtSalida";
+            this.txtSalida.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtSalida.Size = new System.Drawing.Size(550, 350);
+            this.txtSalida.TabIndex = 4;
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.BackColor = System.Drawing.Color.GhostWhite;
+            this.btnLimpiar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnLimpiar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLimpiar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpiar.Location = new System.Drawing.Point(688, 523);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(123, 43);
+            this.btnLimpiar.TabIndex = 5;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = false;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.BackColor = System.Drawing.Color.GhostWhite;
+            this.btnSalir.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSalir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSalir.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalir.Location = new System.Drawing.Point(843, 523);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(130, 43);
+            this.btnSalir.TabIndex = 6;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = false;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(701, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(261, 36);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Salida de Tokens";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(638, 377);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 46);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "=";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(36, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(449, 46);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "ANALIZADOR LÉXICO";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(1045, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(72, 20);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Equipo 2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1329, 745);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.txtSalida);
+            this.Controls.Add(this.btnAnalizar);
+            this.Controls.Add(this.txtEntrada);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Analizador Léxico Versión 1.0 ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtEntrada;
+        private System.Windows.Forms.Button btnAnalizar;
+        private System.Windows.Forms.TextBox txtSalida;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ AnalizadorLexico/Form1.cs
@@ -0,0 +1,325 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AnalizadorLexico
+{
+    public partial class Form1 : Form
+    {
+
+        //   Token1.     Palabra reservada   int, float, double, ....
+        //   Token2.     Identificador (variable)
+        //   Token3.     Numero entero
+        //   Token4.     Numero con punto decimal 
+        //   Token5.     Operador Aritmetico   +  -  *   /  ^   
+        //   Token6.     Operador Relacional   >  >=  <   <=   !=   ==
+        //   Token7.     Operador Logico   &&    ||   ~  
+        //   Token8.     Asignacion    =
+        //   Token9.     Fin de instruccion     ;             
+        //   Token10.    Separador    ,                        
+        //   Token11.    Parentesis   (   )                    
+        //   Token12.    Corchetes    [   ]                    
+        //   Token13.    Llaves       {   }                    
+        //   Token14.    Cadena       "Hola"                   
+        //   Token15.    Caracter     'z'                      
+        //   Token16.    dos puntos  :                        
+        //   Token17.    incremento   ++                      
+        //   Token18.    Decremento   --                       
+        //   Token19.    Operador aritmetivo inclusivo    +=  -=  *=  /=    
+        //   Token20.    Punto   .
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            txtEntrada.Clear();
+            txtSalida.Clear();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnAnalizar_Click(object sender, EventArgs e)
+        {
+            string[] PalRes = { "int", "float","print" ,"break", "double", "char", "string", "for", "if", "while", "else", "do", "then"};
+            string Buffer = txtEntrada.Text;
+            int maximo = Buffer.Length , n=0;
+            char ch;
+            string lexema, salida = string.Empty;
+
+
+            for (int ap = 0; ap < maximo; ap++)
+            {
+                ch = Buffer[ap];
+                lexema = string.Empty;
+                if (char.IsWhiteSpace(ch)) { }
+                else if (char.IsLetter(ch))    // Token 1 y 2 Es una palabra reservada o un identificador
+                {
+                    while (char.IsLetterOrDigit(ch) || ch == '_')
+                    {
+                        lexema += ch;
+                        ap++; if (ap >= maximo) break;
+                        ch = ch = Buffer[ap];
+                    }
+                    ap--;
+                    bool encontrado = false; ;
+                    for (int i = 0; i < PalRes.Length; i++) if (PalRes[i] == lexema) encontrado = true;
+                    if (encontrado) salida += "Token1 Palabra reservada --> " + lexema + "\r\n";
+                    else salida += "Token2 Identificador --> " + lexema + "\r\n";
+                }
+
+                else if (char.IsNumber(ch))  //Token 3 y 4  Es un numero entero o con punto decimal
+                {
+                    while (char.IsNumber(ch) || ch == '.')
+                    {
+                        lexema += ch;
+                        ap++;
+                        if (ap >= maximo) break;
+                        ch = Buffer[ap];
+                        if (ch == '.') n = 1;
+                    }
+                    if (n == 1)
+                    {
+                        salida += "Token4  Numero con Punto Decimal --> " + lexema + "\r\n";
+                    }
+                    else
+                    {
+                        salida += "Token3  Numero Entero --> " + lexema + "\r\n";
+                        ap--;
+                    }
+                }
+
+                else if (ch == '+') // Token5 Operador Aritmetico
+                {
+                    lexema += ch;
+                    ap++;
+                    if (ap >= maximo) break;
+                    ch = Buffer[ap];
+
+                    if (ch == '=') // Token 19 Operador Aritmetico Inclusivo
+                    {
+                        lexema += ch;
+                        salida += "Token19 Operador Aritmetico Inclusivo -->" + lexema + "\r\n";
+                    }
+                    else if (ch == '+') // Token17 Incremento
+                    {
+                        lexema += ch;
+                        salida += "Token17 Incremento -->" + lexema + "\r\n";
+                    }
+                    else
+                    {
+                        lexema += ch;
+                        salida += "Token5 Operador Aritmetico -->" + lexema + "\r\n";
+                    }
+                }
+
+                else if (ch == '-')
+                {
+                    lexema += ch;
+                    ap++;
+                    if (ap >= maximo) break;
+                    ch = Buffer[ap];
+
+                    if (ch == '=') // Token19 Operador Aritmetico Inclusivo
+                    {
+                        lexema += ch;
+                        salida += "Token19 Operador Aritmetico Inclusivo -->" + lexema + "\r\n";
+                    }
+                    else if (ch == '-')  // Token18 Decremento
+                    {
+                        lexema += ch;
+                        salida += "Token18 Decremento -->" + lexema + "\r\n";
+                    }
+                    else
+                    {
+                        lexema += ch;
+                        salida += "Token5 Operador Aritmetico -->" + lexema + "\r\n";
+                    }
+                }
+
+                else if ( ch == '*' || ch == '/')   // Token 5  Operador Aritmetico
+                {
+                    lexema += ch;
+                    ap++;
+                    if (ap >= maximo) break;
+                    ch = Buffer[ap];
+
+                    if (ch == '=') // Token 19 Operador Aritmetico Inclusivo
+                    {
+                        lexema += ch;
+                        salida += "Token19 Operador Aritmetico Inclusivo -->" + lexema + "\r\n";
+                    }
+                    else
+                    {
+                        lexema += ch;
+                        salida += "Token5 Operador Aritmetico -->" + lexema + "\r\n";
+                    }
+                }
+
+                else if (ch == '^') // Token5 Operador Aritmetico potencia
+                {
+                    lexema += ch;
+                    salida += "Token5 Operador Aritmetico -->" + lexema + "\r\n";
+                }
+
+                else if (ch == '>' || ch == '<' || ch == '!' || ch == '=')   //  Token 6 y 8
+                {
+                    lexema += ch;
+                    ap++; if (ap >= maximo) break;
+                    ch = Buffer[ap];
+                    if (ch == '=') salida += "Token6 Operador Relacional --> " + lexema + "=" + "\r\n";
+                    else
+                    {
+                        ap--;
+                        if (lexema == ">" || lexema == "<") salida += "Token6 Operador Relacional --> " + lexema + "\r\n";
+                        else if (lexema == "=") salida += "Token8 Asignacion --> " + lexema + "\r\n";  // Token 8
+                        else salida += "Error --> " + lexema + "\r\n";
+                    }
+                }
+
+
+                else if (ch == '|')   // Token7 Operador logico  OR
+                {
+                    lexema += ch;
+                    ap++; if (ap >= maximo) break;
+                    ch = Buffer[ap];
+
+                    if (ch == '|') // Token7 Operador Logico
+                    {
+                        lexema += ch;
+                        salida += "Token7 Operador Logico -->" + lexema + "\r\n";
+                    }
+                    else
+                    {
+                        lexema += ch;
+                        salida += "ERROR DE SINTAXIS ------>" + lexema + "\r\n";
+                    }
+                }
+
+                else if (ch == '&') // Token7 Operador Logico AND
+                {
+                    lexema += ch;
+                    ap++; if (ap >= maximo) break;
+                    ch = Buffer[ap];
+
+                    if (ch == '&') // Token 7 Operador Logico
+                    {
+                        lexema += ch;
+                        salida += "Token7 Operador Logico -->" + lexema + "\r\n";
+                    }
+                    else
+                    {
+                        lexema += ch;
+                        salida += "ERROR DE SINTAXIS ------>" + lexema + "\r\n";
+                    }
+                }
+
+                else if (ch == '~')  // Token7   Operador Logico NOT
+                {
+                    lexema += ch;
+                    salida += "Token7  Operador Logico --> " + lexema + "\r\n";
+                }
+
+                else if (ch == ';')  // Token 9   Fin de la Instruccion
+                {
+                    lexema += ch;
+                    salida += "Token9  Fin de la Instruccion --> " + lexema + "\r\n";
+                }
+
+                else if (ch == ',') // Token 10 Separador
+                {
+                    lexema += ch;
+                    salida += "Token10  Separador --> " + lexema + "\r\n";
+                }
+
+
+                else if (ch == '(' || ch == ')')  // Token 11 Parentesis
+                {
+                    lexema += ch;
+                    salida += "Token11  Parentesis --> " + lexema + "\r\n";
+                }
+
+                else if (ch == '[' || ch == ']')  // Token 12 Corchetes
+                {
+                    lexema += ch;
+                    salida += "Token12  Corchete --> " + lexema + "\r\n";
+                }
+
+                else if (ch == '{' || ch == '}')  // Token 13 Llaves
+                {
+                    lexema += ch;
+                    salida += "Token13  Llaves --> " + lexema + "\r\n";
+                }
+               
+                else if (ch == '"') // Token 14  Cadena
+                {
+                    lexema += ch;
+                    ap++;
+                    if (ap >= maximo) break;
+                    ch = Buffer[ap];
+
+                    while (char.IsLetterOrDigit(ch) || char.IsWhiteSpace(ch))
+                    {
+                        lexema += ch;
+                        ap++;
+                        if (ap >= maximo) break;
+                        ch = Buffer[ap];
+                        if (ch == '"')
+                        {
+                            lexema += ch;
+                            break;
+                        }
+                    }
+                    salida += "Token14 Cadena -->" + lexema + "\r\n";
+                }
+
+                else if (ch == '\'')//Token15 Carácter
+                {
+                    lexema += ch;
+                    ap++;
+                    if (ap >= maximo) break;
+                    ch = Buffer[ap];
+                    if (char.IsLetterOrDigit(ch))
+                        lexema += ch;
+                    ap++;
+                    if (ap >= maximo) break;
+                    ch = Buffer[ap];
+                    salida += "Token 15 Caracter --> " + lexema + ch + "\r\n";
+                }
+
+                else if (ch == ':')  // Token 16 Dos Puntos
+                {
+                    lexema += ch;
+                    salida += "Token16  Dos Puntos --> " + lexema + "\r\n";
+                }
+
+                else if (ch == '.')    //  Token 20
+                {
+                    salida += "Token20 Punto --> ." + "\r\n";
+                }
+            }
+            txtSalida.Text = salida;
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
